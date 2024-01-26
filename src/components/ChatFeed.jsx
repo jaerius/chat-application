963b5e54815806179dbf6f4d47897e6444a3cee7
jaerius <rylynn1029@naver.com>
2024-01-26 18:30:55 +0900

    chatfeed and message form edited but still some errors

--- src/components/ChatFeed.jsx
+++ src/components/ChatFeed.jsx
@@ -7,6 +7,19 @@ const ChatFeed = (props) => {
 
     const chat = chats && chats[activeChat]; //if chats exists then find chats[activeChat]
 
+    const renderReadReceipts = (message, isMyMessage) => {
+        chat.people.map((person, index)=> person.last_read === message.id && (
+            <div 
+                key= {`read_${index}`}
+                className="read-receipt"
+                style={{
+                    float: isMyMessage ? 'right': 'left',
+                    backgroundImage: `url(${person?.person?.avatar})`
+                }}
+            />
+        ))
+    }
+
     const renderMessages = () => {
         const keys = Object.keys(messages);
         return keys.map((key, index)=>{
@@ -24,7 +37,7 @@ const ChatFeed = (props) => {
                         }
                     </div>
                     <div className="read-receipts" style={{marginRight: isMyMessage ? '18px': '0px', marginLeft: isMyMessage? '0px' : '68px' }}>
-                        
+                        {renderReadReceipts(message, isMyMessage)}
                     </div>
                 </div>
                 
